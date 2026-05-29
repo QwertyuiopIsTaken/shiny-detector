537dd2b3055032d796cd8435912d918c0be0c9c4
QwertyuiopIsTaken <ricky85125@gmail.com>
2026-05-29 00:48:06 -0400

    Removed detector class

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from state import AppState
-from detector import ShinyDetector
 from screen_match import ScreenMatcher
 from controller import DetectorController
 from gui import SoundDetectorGUI
 
 if __name__ == "__main__":
     state = AppState()
-    detector = ShinyDetector(state)
     matcher = ScreenMatcher()
-    controller = DetectorController(state, detector, matcher)
+    controller = DetectorController(state, matcher)
     SoundDetectorGUI(state, controller)
